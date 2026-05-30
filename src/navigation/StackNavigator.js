16374bd50407ec413425c34c9b9b974b85618977
avasconcelos-tech <a.vasconcelos@edu.senai.br>
2026-05-30 11:45:16 -0300

    testes

--- src/navigation/StackNavigator.js
+++ src/navigation/StackNavigator.js
@@ -1,15 +1,15 @@
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from '../screens/HomeScreen';
+import DetalheScreen from '../screens/DetalheScreen';
 
-import DetalheScreen from "../screens/DetalheScreen";
-import HomeScreen from "../screens/HomeScreen";
-
-const Stack = createNativeStackNavigator();
+const Stack = createStackNavigator();
 
 export default function StackNavigator() {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
-       <Stack.Screen name="Home" component={HomeScreen} />
-      <Stack.Screen name="Detalhe" component={DetalheScreen} />
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Detalhe" component={DetalheScreen} /> 
     </Stack.Navigator>
   );
 }
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -124,7 +124,7 @@ export default function HomeScreen({ navigation }) {
             genero={item.genero}
             plataforma={item.plataforma}
             nota={item.nota}
-            onPress={() => alert("O clique funcionou na HomeScreen!")}
+            onPress={() => navigation.navigate('Detalhe', { ...item })}
           />
         )}
         contentContainerStyle={styles.lista}
