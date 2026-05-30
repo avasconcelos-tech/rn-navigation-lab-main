437cc4852d4403c6b0cb69100d2a0428f7f031fd
avasconcelos-tech <a.vasconcelos@edu.senai.br>
2026-05-30 10:38:25 -0300

    update app.js

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
-import { NavigationContainer } from "@react-navigation/native";
-import { SafeAreaProvider } from "react-native-safe-area-context";
-import AppNavigator from "./src/navigation";
+import React from 'react';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { NavigationContainer } from '@react-navigation/native';
+import AppNavigator from './src/navigation'; // Importa o index.js da navegação
 
 export default function App() {
   return (
     <SafeAreaProvider>
-      {<NavigationContainer/>}
-      {<AppNavigator/>}
+      <NavigationContainer>
+        <AppNavigator />
+      </NavigationContainer>
     </SafeAreaProvider>
   );
 }
